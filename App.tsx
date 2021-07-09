422015bfeaf65ab13d201125e2f1a6a21323d4ec
Chieftainroma <romanmammadov872@gmail.com>
2021-07-09 18:06:06 +0400

    implement auth

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
+import {
+  Auth,
+  API,
+} from 'aws-amplify';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -13,6 +17,18 @@ import { withAuthenticator } from 'aws-amplify-react-native'
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() =>{
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      console.log(userInfo);
+
+      if(userInfo){
+
+      }
+    }
+    fetchUser();
+  },[])
 
   if (!isLoadingComplete) {
     return null;
